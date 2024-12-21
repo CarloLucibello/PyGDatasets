560da192374b8de333e584afe4353d24278c997c
Carlo Lucibello <carlo.lucibello@gmail.com>
2024-12-21 01:25:21 +0100

    update

--- README.md
+++ README.md
@@ -2,8 +2,48 @@
 
 [![Build Status](https://github.com/CarloLucibello/PyGDatasets.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/CarloLucibello/PyGDatasets.jl/actions/workflows/CI.yml?query=branch%3Amain)
 
+This package provides a Julia interface to the datasets available in [PyTorch Geometric](https://pytorch-geometric.readthedocs.io/en/stable/modules/datasets.html).
+
+## Installation
+
+```julia
+pkg> add PyGDatasets
+```
+
+## Usage
+
+```julia
+julia> using PyGDatasets
+
+julia> dataset = load_dataset("TUDataset", name="MUTAG")
+TUDataset(MUTAG) - InMemoryGNNDataset
+  num_graphs: 188
+  node_features: [:x]
+  edge_features: [:edge_attr]
+  graph_features: [:y]
+  root: /Users/carlo/.julia/scratchspaces/44f67abd-f36e-4be4-bfe5-65f468a62b3d/datasets/TUDataset
+
+julia> g = dataset[1]
+GNNGraph:
+  num_nodes: 17
+  num_edges: 38
+  ndata:
+    x = 7×17 Matrix{Float32}
+  edata:
+    edge_attr = 4×38 Matrix{Float32}
+  gdata:
+    y = 1-element Vector{Int64}
+```
+Other examples:
+```julia
+load_dataset("KarateClub")
+load_dataset("GNNBenchmarkDataset", name="CSL")
+load_dataset("ZINC", subset=true, split="test")
+load_dataset("Planetoid", root="./", name="Cora")
+load_dataset("MoleculeNet", name="ESOL")
+```
 
 ## References
 
-- [Dataset Cheatsheet](https://pytorch-geometric.readthedocs.io/en/stable/notes/data_cheatsheet.html)
-- [Dataset Documentation](https://pytorch-geometric.readthedocs.io/en/stable/modules/datasets.html)
+- [PyG Datasets Cheatsheet](https://pytorch-geometric.readthedocs.io/en/stable/notes/data_cheatsheet.html)
+- [PyG Datasets Documentation](https://pytorch-geometric.readthedocs.io/en/stable/modules/datasets.html)
--- test/datasets.jl
+++ test/datasets.jl
@@ -19,6 +19,47 @@
     @test g.train_mask == pyconvert(Any, g_py.train_mask.numpy())
     @test g.num_nodes == pyconvert(Int, g_py.num_nodes)
     @test g.num_edges == pyconvert(Int, g_py.num_edges)
-
 end
 
+@testitem "Cora" setup=[TestModule] begin
+    using .TestModule
+    dataset = load_dataset("Planetoid", name="Cora")
+    @test dataset.num_graphs == 1
+    @test dataset.node_features == [:val_mask, :test_mask, :y, :train_mask, :x] 
+    g = dataset[1]
+    @test g isa GNNGraph
+    @test g.num_nodes == 2708
+    @test g.num_edges == 10556
+    @test g.y isa Vector{Int}
+    @test g.x isa Matrix{Float32}
+    @test g.train_mask isa Vector{Bool}
+    @test g.val_mask isa Vector{Bool}
+    @test g.test_mask isa Vector{Bool}
+    @test size(g.x) == (1433, 2708)
+end
+
+@testitem "FreeSolv" setup=[TestModule] begin
+    using .TestModule
+    dataset = load_dataset("MoleculeNet", name="FreeSolv")
+    @test dataset.num_graphs == 642
+    @test dataset.node_features == [:x]
+    @test dataset.edge_features == [:edge_attr]
+    @test dataset.graph_features == [:y, :smiles]
+    @test length(dataset.graphs) == 642
+    g = dataset[1]
+    @test g.x isa Matrix{Int}
+    @test size(g.x) == (9, 13)
+    @test g.x == [6  7  6  6  8  6  6  6  6  6  6  8  6
+                0  0  0  0  0  0  0  0  0  0  0  0  0
+                4  3  4  3  1  3  3  3  3  3  3  2  4
+                5  5  5  5  5  5  5  5  5  5  5  5  5
+                3  0  3  0  0  0  1  1  0  1  1  0  3
+                0  0  0  0  0  0  0  0  0  0  0  0  0
+                4  3  4  3  3  3  3  3  3  3  3  3  4
+                0  0  0  0  0  1  1  1  1  1  1  0  0
+                0  0  0  0  0  1  1  1  1  1  1  0  0]
+    @test g.edge_attr isa Matrix{Int}
+    @test size(g.edge_attr) == (3, 26)
+    @test g.gdata.y isa Matrix{Float32}
+    @test size(g.gdata.y) == (1, 1)
+end
